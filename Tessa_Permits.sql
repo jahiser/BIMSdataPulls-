0ed768bb95c2b0aca1faebe9a39028277f0e5c25
jahiser <45430584+jahiser@users.noreply.github.com>
2018-11-28 10:38:37 -0500

    Add files via upload

---
+++ Tessa_Permits.sql
@@ -0,0 +1,93 @@
+ -- Duplicates beacuse of regulated activities 
+ -- Cannot do inpsctions because it's causing duplicats for ras 
+ -- Cannot do field and Dry tons together 
+ SELECT distinct  P.NUM as PERMIT, 
+ date(p.EFFECTIVEDATE) as PERMIT_ISSUED_DT,
+ PT.desc as PERMIT_TYPE,   
+ C.NAME as COUNTY, 
+ R.name as REGION,
+ FCTY.Name as  facility,
+ case when FS.refcode = 'ACTIVE  ' then 1 else 0 end as FACILITY_ACTIVE,
+ FS.desc as FACILITY_STATUS,
+
+CASE WHEN O.ORGANIZATIONNAME IS NULL THEN RTRIM(O.FIRSTNAME) || CASE WHEN O.MIDDLENAME IS NULL 
+           THEN ' ' ELSE ' ' || RTRIM( O.MIDDLENAME) END || ' '|| RTRIM(O.LASTNAME) ELSE O.ORGANIZATIONNAME END AS Owner ,
+
+FLOC.locationLatNum        as  Lat ,
+FLOC.locationLongNum        as  Long,
+US.FIRSTNAME || ' ' || US.lastname as PRIMARY_REVIEWER,
+Max(date(INSPECTIONENTRYDT)) as LAST_INSPECTION_DT,
+p.PRMTTEDFLOWQTYGPD as PERMITED_FLOW, 
+P.ASBUILTFLOWQTYGPD as AS_BUILT_FLOW, 
+p.DOMESTICPCT as Domestic,
+P.STORMWATERPCT AS Stormwater,
+P.INDUSTRIALPCT as industrial,
+date(P.ORIGINALISSUEDDATE) as Original_Issued_DT, 
+sum(f.ACREAGE) as FIELD_ACRES,
+sum(RS.ESTDRYTONSQTY) as DRY_TONS,
+--,RA.desc,
+        
+
+
+''                 as ECM_LINK,
+' '                as BlankField_1,
+' '                as BlankField_2
+
+
+FROM ((((( (((((( ((((  ((( (
+
+	Permit P JOIN PermitType PT on PT.id = P.permitTypeId and  programcategoryid ='2W1WFMYG0006KD84EKJ3RBNABT' and P.num = 'WQ0000520'
+                                                              and refcode not in ('COLLECT ','DEEMED  ','GRAVITY ','CERTAPPR','PUMP    ','GWREMNON','DSTRANML'))
+	JOIN PERMITSTATUS PS on P.permitStatusId = PS.ID and  PS.REFCODE in ('ACTIVE','EXPIRED'))
+	JOIN Facility FCTY on FCTY.iD = P.FacilityID)
+	JOIN OWNER O on O.iD = FCTY.OWNERID)
+        JOIN COUNTY C on C.iD = FCTY.COUNTYID) 
+        JOIN REGION R on P.adminRegionId = R.id)
+        JOIN STATUS FS on FS.id = FCTY.FACILITYSTATUSID)
+        JOIN PERMITREVIEWER PR on PR.permitid = P.id and PR.ISPRIMARY =1)
+        JOIN USER US on US.id = PR.userid) 
+        JOIN PERMITFACREGACTVTY PFRA on  PFRA.PERMITID = P.id)
+        JOIN FACILITYREGACTVTY FRA on FRA.Id = PFRA.FCLTYREGACTVTYID)
+        JOIN REGACTIVITYTYPE RA on RA.id = FRA.REGACTIVITYTYPEID)
+        LEFT OUTER JOIN   ENVINTLOCATION  FLOC ON FLOC.ENVINTID  = FCTY.id)
+        LEFT OUTER JOIN   INSPECTION   I ON I.PERMITID  = P.id)
+        LEFT OUTER JOIN INSPECTIONAFFILIATION IA on IA.inspectionid = I.id)        
+        LEFT OUTER JOIN  PermitEnvInt pei on PEI.permitId = P.id)
+        LEFT OUTER JOIN   EnvInt ei  on pei.envIntId = ei.id AND pei.expirationDt is null)
+        LEFT OUTER JOIN Field f  on f.id  = ei.id)
+        LEFT OUTER JOIN RESIDUALSOURCE RS  on RS.permitid  = P.id)
+                                                      
+ 
+       
+        
+      
+ Group By 
+  P.NUM ,
+ date(p.EFFECTIVEDATE) ,
+ PT.desc ,  
+ C.NAME ,
+ R.name, 
+ FCTY.Name , 
+    FS.refcode,
+ FS.desc ,
+ 
+CASE WHEN O.ORGANIZATIONNAME IS NULL THEN RTRIM(O.FIRSTNAME) || CASE WHEN O.MIDDLENAME IS NULL 
+           THEN ' ' ELSE ' ' || RTRIM( O.MIDDLENAME) END || ' '|| RTRIM(O.LASTNAME) ELSE O.ORGANIZATIONNAME END   ,
+FLOC.locationLatNum,        
+FLOC.locationLongNum ,
+US.FIRSTNAME || ' ' || US.lastname ,
+ p.PRMTTEDFLOWQTYGPD,
+P.ASBUILTFLOWQTYGPD, 
+p.DOMESTICPCT ,
+P.STORMWATERPCT , 
+P.INDUSTRIALPCT   ,
+P.ORIGINALISSUEDDATE
+
+
+	 
+	 
+ 
+
+ 
+ 		 
+ 
