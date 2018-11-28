87ee7b6270a39152f514fb9a8d56ee4f2930b463
jahiser <45430584+jahiser@users.noreply.github.com>
2018-11-28 10:47:27 -0500

    Update Tessa_Permits.sql

--- Tessa_Permits.sql
+++ Tessa_Permits.sql
@@ -1,8 +1,11 @@
  -- Duplicates beacuse of regulated activities 
  -- Cannot do inpsctions because it's causing duplicats for ras 
  -- Cannot do field and Dry tons together 
+ -- Cannot do relate permit because of duplicates 
+ -- updated 11-28-2018 to include Tessa's lastest request 
  
- SELECT distinct  P.NUM as PERMIT, 
+ SELECT distinct  
+ P.NUM as PERMIT, 
  date(p.EFFECTIVEDATE) as PERMIT_ISSUED_DT,
  PT.desc as PERMIT_TYPE,   
  C.NAME as COUNTY, 
@@ -17,7 +20,7 @@ CASE WHEN O.ORGANIZATIONNAME IS NULL THEN RTRIM(O.FIRSTNAME) || CASE WHEN O.MIDD
 FLOC.locationLatNum        as  Lat ,
 FLOC.locationLongNum        as  Long,
 US.FIRSTNAME || ' ' || US.lastname as PRIMARY_REVIEWER,
-Max(date(INSPECTIONENTRYDT)) as LAST_INSPECTION_DT,
+
 p.PRMTTEDFLOWQTYGPD as PERMITED_FLOW, 
 P.ASBUILTFLOWQTYGPD as AS_BUILT_FLOW, 
 p.DOMESTICPCT as Domestic,
@@ -26,7 +29,7 @@ P.INDUSTRIALPCT as industrial,
 date(P.ORIGINALISSUEDDATE) as Original_Issued_DT, 
 sum(f.ACREAGE) as FIELD_ACRES,
 sum(RS.ESTDRYTONSQTY) as DRY_TONS,
---,RA.desc,
+
         
 
 
@@ -35,9 +38,9 @@ sum(RS.ESTDRYTONSQTY) as DRY_TONS,
 ' '                as BlankField_2
 
 
-FROM ((((( (((((( ((((  ((( (
+FROM ((((( (((((( ((((  ((
 
-	Permit P JOIN PermitType PT on PT.id = P.permitTypeId and  programcategoryid ='2W1WFMYG0006KD84EKJ3RBNABT' and P.num = 'WQ0000520'
+	Permit P JOIN PermitType PT on PT.id = P.permitTypeId and  programcategoryid ='2W1WFMYG0006KD84EKJ3RBNABT' 
                                                               and refcode not in ('COLLECT ','DEEMED  ','GRAVITY ','CERTAPPR','PUMP    ','GWREMNON','DSTRANML'))
 	JOIN PERMITSTATUS PS on P.permitStatusId = PS.ID and  PS.REFCODE in ('ACTIVE','EXPIRED'))
 	JOIN Facility FCTY on FCTY.iD = P.FacilityID)
@@ -51,8 +54,7 @@ FROM ((((( (((((( ((((  ((( (
         JOIN FACILITYREGACTVTY FRA on FRA.Id = PFRA.FCLTYREGACTVTYID)
         JOIN REGACTIVITYTYPE RA on RA.id = FRA.REGACTIVITYTYPEID)
         LEFT OUTER JOIN   ENVINTLOCATION  FLOC ON FLOC.ENVINTID  = FCTY.id)
-        LEFT OUTER JOIN   INSPECTION   I ON I.PERMITID  = P.id)
-        LEFT OUTER JOIN INSPECTIONAFFILIATION IA on IA.inspectionid = I.id)        
+     
         LEFT OUTER JOIN  PermitEnvInt pei on PEI.permitId = P.id)
         LEFT OUTER JOIN   EnvInt ei  on pei.envIntId = ei.id AND pei.expirationDt is null)
         LEFT OUTER JOIN Field f  on f.id  = ei.id)
